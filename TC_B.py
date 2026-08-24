e51197252a172de70fbfe4eebdf69a951b3565ae
moweather <zax41006@gmail.com>
2026-08-24 09:22:33 +0800

    Update TC_B.py

--- TC_B.py
+++ TC_B.py
@@ -90,7 +90,7 @@ def draw_chart():
         fig, ax = plt.subplots(figsize=(12, 10), subplot_kw={'projection': ccrs.PlateCarree()})
         
         # 根據過去與預報經緯度動態或固定邊界
-        lon_min, lon_max, lat_min, lat_max = 105.0, 165.0, 5, 55
+        lon_min, lon_max, lat_min, lat_max = 105.0, 145.0, 15, 45
         ax.set_extent([lon_min, lon_max, lat_min, lat_max], crs=ccrs.PlateCarree())
 
 
